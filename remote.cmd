dff42f08923c14315085a1e35e9f597324d88d73
Grctsamu <98705451+Grctsamu@users.noreply.github.com>
2023-04-18 15:37:29 -0700

    Update remote.cmd

--- remote.cmd
+++ remote.cmd
@@ -5,6 +5,7 @@ if not exist %cd%\env\ (
 md env
 )
 del %cd%\env\* /Q
+
 rem set /p "fl=>
 set fl=%1
 curl "https://raw.githubusercontent.com/Grctsamu/remotebat/main/scripts/%fl%.bat" > %cd%\env\e.bat
@@ -12,10 +13,12 @@ cls
 echo Executing %fl% 
 timeout 1 /NOBREAK > nul
 cls
-call %cd%\env\e.bat
+cd env
+call e.bat
 echo.
 echo.
 echo End
+cd..
 del %cd%\env\ /Q
 color
 pause
