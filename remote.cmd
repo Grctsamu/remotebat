cc1d5b94cc1d48d6920f7a7ef7190f5d9585e0ba
Grctsamu <98705451+Grctsamu@users.noreply.github.com>
2023-04-18 15:13:58 -0700

    Update remote.cmd

--- remote.cmd
+++ remote.cmd
@@ -6,7 +6,13 @@ md env
 )
 del %cd%\env\* /Q
 curl "https://raw.githubusercontent.com/Grctsamu/remotebat/main/scripts/%fl%.bat" > %cd%\env\e.bat
+cls
 echo Executing %fl% 
+timeout 1 /NOBREAK > nul
+cls
 call %cd%\env\e.bat
+echo.
+echo.
 echo End
 del %cd%\env\ /Q
+pause
