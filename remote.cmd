243636c1e3437154c740e13bede65d628a8848e2
Grctsamu <98705451+Grctsamu@users.noreply.github.com>
2023-04-18 15:08:13 -0700

    Update remote.cmd

--- remote.cmd
+++ remote.cmd
@@ -1,6 +1,12 @@
 @echo off
 set /p "fl=>
-curl %fl% > e.bat
+
+if not exists %cd%\env\ (
+md env
+)
+del %cd%\env\* /Q
+curl "https://raw.githubusercontent.com/Grctsamu/remotebat/main/scripts/%fl%" > %cd%\env\e.bat
 echo Executing %fl% 
-call e.bat
+call %cd%\env\e.bat
 echo End
+del %cd%\env\ /Q
