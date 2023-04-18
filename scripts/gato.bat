f2e519d11467334e551b88778c161abbc041692c
Grctsamu <98705451+Grctsamu@users.noreply.github.com>
2023-04-18 15:41:52 -0700

    Update gato.bat

--- scripts/gato.bat
+++ scripts/gato.bat
@@ -1,264 +1,358 @@
-@ECHO OFF
-REM " AQUI MIRA SI SE ESTA INICIANDO EL JUEGO Y SI ES ASI BORRA
-REM TODA HUELLA DE ALGUNA POSIBLE SALIDA INCORRECTA DEL JUEGO ".
-@IF A == %1 GOTO BEGIN
-@IF X == %1 GOTO BEGIN
-@IF O == %1 GOTO BEGIN
-IF EXIST *.T DEL *.T
-:BEGIN
-REM " EN ESTA PARTE MIRA Y ACTUALIZA LA CANTIDAD DE JUGADAS YA REALIZADAS "
-IF NOT EXIST *.T CLS
-IF EXIST 9.T REN 9.T 10.T
-IF EXIST 10.T GOTO INICIO
-IF EXIST 8.T REN 8.T 9.T
-IF EXIST 9.T GOTO INI
-IF EXIST 7.T REN 7.T 8.T
-IF EXIST 8.T GOTO INI
-IF EXIST 6.T REN 6.T 7.T
-IF EXIST 7.T GOTO INI
-IF EXIST 5.T REN 5.T 6.T
-IF EXIST 6.T GOTO INI
-IF EXIST 4.T REN 4.T 5.T
-IF EXIST 5.T GOTO INI
-IF EXIST 3.T REN 3.T 4.T
-IF EXIST 4.T GOTO INI
-IF EXIST 2.T REN 2.T 3.T
-IF EXIST 3.T GOTO INI
-IF EXIST 1.T REN 1.T 2.T
-IF EXIST 2.T GOTO INI
-IF EXIST 0.T REN 0.T 1.T
-IF EXIST 1.T GOTO INI
-REM " ESTA ES LA PORTADA DE BIENVENIDA "
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO                        BIENVENIDO A: " BATCHRUN TRIQUI "
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO                            DISE¥ADO POR: BLACKWOLF
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO                         TODOS LOS DERECHOS RESERVADOS
-IF NOT EXIST 0.T ECHO.
-IF NOT EXIST 0.T ECHO                             COLOMBIA COFFEE HACKERS
-IF NOT EXIST 0.T PAUSE > NUL
-IF NOT EXIST 0.T ECHO TRIQUI > 0.T
-REM "AQUI SE CREA UN SUBPROGRAMITA QUE USARA EL JUEGO PARA PODER LEER
-REM LAS OPCIONES QUE TECLEE EL JUGADOR, SIEMPRE Y CUANDO NO EXISTA "
-IF EXIST CODTECLA.COM GOTO NDEBUG
-:DEBUG
-ECHO N CODTECLA.COM >> CT.SCR
-ECHO A 100 >> CT.SCR
-ECHO INT 16 >> CT.SCR
-ECHO MOV AL, AH >> CT.SCR
-ECHO MOV AH, 4C  >> CT.SCR
-ECHO INT 21  >> CT.SCR
-ECHO.  >> CT.SCR
-ECHO R CX  >> CT.SCR
-ECHO 8  >> CT.SCR
-ECHO W  >> CT.SCR
-ECHO Q  >> CT.SCR
-DEBUG < CT.SCR > NUL
-DEL CT.SCR
-:NDEBUG
-REM " AQUI SE INICIA Y ASIGNAN LOS PRIMEROS VALORES A LA MATRIZ DEL JUEGO "
-%0 A B C D E F G H I
-REM " EN ESTA PARTE MIRA CON QUE SIGNO (X,O) HACER LA JUGADA "
-:INI
-IF NOT EXIST X.T GOTO INIA
-IF EXIST X.T GOTO INIB
-:INIA
-ECHO TRIQUI > X.T
-GOTO INICIO
-:INIB
-DEL X.T
-GOTO INICIO
-REM " AQUI SE MUESTRA EN PANTALLA LA MATRIZ CON LOS VALORES QUE SE TRAIGAN "
-:INICIO
-CLS
-ECHO.
-ECHO.
-ECHO                  TECLEE LA LETRA EN DONDE QUIERE HACER SU JUGADA :
-ECHO.
-ECHO.
-ECHO                                   ÉÍÍÍËÍÍÍËÍÍÍ»
-ECHO                                   º %1 º %2 º %3 º
-ECHO                                   ÌÍÍÍÎÍÍÍÎÍÍÍ¹
-ECHO                                   º %4 º %5 º %6 º
-ECHO                                   ÌÍÍÍÎÍÍÍÎÍÍÍ¹
-ECHO                                   º %7 º %8 º %9 º
-ECHO                                   ÈÍÍÍÊÍÍÍÊÍÍÍ¼
-ECHO.
-ECHO                             O PULSE " ESC " PARA SALIR.
-ECHO.
-REM " EN ESTA PARTE SE COMPARAN LOS VALORES DE LA MATRIZ DEL JUEGO
-REM PARA SABER SI ALGUIEN GANO Y SE FELICITA A QUIEN LO HIZO"
-IF %1 == %2 IF %2 == %3 ECHO 
-IF %1 == %2 IF %2 == %3 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %1 == %2 IF %2 == %3 PAUSE > NUL
-IF %1 == %2 IF %2 == %3 GOTO FIN
-IF %4 == %5 IF %5 == %6 ECHO 
-IF %4 == %5 IF %5 == %6 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %4 == %5 IF %5 == %6 PAUSE > NUL
-IF %4 == %5 IF %5 == %6 GOTO FIN
-IF %7 == %8 IF %8 == %9 ECHO 
-IF %7 == %8 IF %8 == %9 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %7 == %8 IF %8 == %9 PAUSE > NUL
-IF %7 == %8 IF %8 == %9 GOTO FIN
-IF %1 == %4 IF %4 == %7 ECHO 
-IF %1 == %4 IF %4 == %7 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %1 == %4 IF %4 == %7 PAUSE > NUL
-IF %1 == %4 IF %4 == %7 GOTO FIN
-IF %2 == %5 IF %5 == %8 ECHO 
-IF %2 == %5 IF %5 == %8 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %2 == %5 IF %5 == %8 PAUSE > NUL
-IF %2 == %5 IF %5 == %8 GOTO FIN
-IF %3 == %6 IF %6 == %9 ECHO 
-IF %3 == %6 IF %6 == %9 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %3 == %6 IF %6 == %9 PAUSE > NUL
-IF %3 == %6 IF %6 == %9 GOTO FIN
-IF %1 == %5 IF %5 == %9 ECHO 
-IF %1 == %5 IF %5 == %9 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %1 == %5 IF %5 == %9 PAUSE > NUL
-IF %1 == %5 IF %5 == %9 GOTO FIN
-IF %7 == %5 IF %5 == %3 ECHO 
-IF %7 == %5 IF %5 == %3 ECHO                         ­ FELICITACIONES ACABAS DE GANAR !
-IF %7 == %5 IF %5 == %3 PAUSE > NUL
-IF %7 == %5 IF %5 == %3 GOTO FIN
-REM " SI EXISTE 10.T SIGNIFICA QUE HIZO LA ULTIMA JUGADA Y SI SE LLEGO A ESTA
-REM  PARTE, ES QUE NADIE GANO, POR LO QUE SE EJECUTA EL FINAL DEL JUEGO "
-IF EXIST 10.T  ECHO                             NADIE GANO EN ESTA OCACION
-IF EXIST 10.T PAUSE > NUL
-IF EXIST 10.T GOTO FIN
-:COD
-REM " EN ESTA PARTE SE LEE LA OPCION QUE DESEA EL JUGADOR ELEGIR "
-CODTECLA
-REM " AQUI SE EJECUTA LA OPCION ELEGIDA POR EL JUGADOR "
-IF ERRORLEVEL 30 IF NOT ERRORLEVEL 31 GOTO A
-IF ERRORLEVEL 48 IF NOT ERRORLEVEL 49 GOTO B
-IF ERRORLEVEL 46 IF NOT ERRORLEVEL 47 GOTO C
-IF ERRORLEVEL 32 IF NOT ERRORLEVEL 33 GOTO D
-IF ERRORLEVEL 18 IF NOT ERRORLEVEL 19 GOTO E
-IF ERRORLEVEL 33 IF NOT ERRORLEVEL 34 GOTO F
-IF ERRORLEVEL 34 IF NOT ERRORLEVEL 35 GOTO G
-IF ERRORLEVEL 35 IF NOT ERRORLEVEL 36 GOTO H
-IF ERRORLEVEL 23 IF NOT ERRORLEVEL 24 GOTO I
-IF ERRORLEVEL 1 IF NOT ERRORLEVEL 2 GOTO FIN
-GOTO INICIO
-REM " EN ESTA PARTE SE ENVIA A LA MATRIZ EL VALOR SELECCIONADO Y
-REM SE MUESTRA EN PANTALLA LA MATRIZ DEL JUEGO COMPLETA "
-:A
-IF %1 == X GOTO INICIO
-IF %1 == x GOTO INICIO
-IF %1 == O GOTO INICIO
-IF %1 == o GOTO INICIO
-IF EXIST X.T %0 X %2 %3 %4 %5 %6 %7 %8 %9
-IF NOT EXIST X.T %0 O %2 %3 %4 %5 %6 %7 %8 %9
-:B
-IF %2 == X GOTO INICIO
-IF %2 == x GOTO INICIO
-IF %2 == O GOTO INICIO
-IF %2 == o GOTO INICIO
-IF EXIST X.T %0 %1 X %3 %4 %5 %6 %7 %8 %9
-IF NOT EXIST X.T %0 %1 O %3 %4 %5 %6 %7 %8 %9
-:C
-IF %3 == X GOTO INICIO
-IF %3 == x GOTO INICIO
-IF %3 == O GOTO INICIO
-IF %3 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 X %4 %5 %6 %7 %8 %9
-IF NOT EXIST X.T %0 %1 %2 O %4 %5 %6 %7 %8 %9
-:D
-IF %4 == X GOTO INICIO
-IF %4 == x GOTO INICIO
-IF %4 == O GOTO INICIO
-IF %4 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 %3 X %5 %6 %7 %8 %9
-IF NOT EXIST X.T %0 %1 %2 %3 O %5 %6 %7 %8 %9
-:E
-IF %5 == X GOTO INICIO
-IF %5 == x GOTO INICIO
-IF %5 == O GOTO INICIO
-IF %5 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 %3 %4 X %6 %7 %8 %9
-IF NOT EXIST X.T %0 %1 %2 %3 %4 O %6 %7 %8 %9
-:F
-IF %6 == X GOTO INICIO
-IF %6 == x GOTO INICIO
-IF %6 == O GOTO INICIO
-IF %6 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 %3 %4 %5 X %7 %8 %9
-IF NOT EXIST X.T %0 %1 %2 %3 %4 %5 O %7 %8 %9
-:G
-IF %7 == X GOTO INICIO
-IF %7 == x GOTO INICIO
-IF %7 == O GOTO INICIO
-IF %7 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 %3 %4 %5 %6 X %8 %9
-IF NOT EXIST X.T %0 %1 %2 %3 %4 %5 %6 O %8 %9
-:H
-IF %8 == X GOTO INICIO
-IF %8 == x GOTO INICIO
-IF %8 == O GOTO INICIO
-IF %8 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 %3 %4 %5 %6 %7 X %9
-IF NOT EXIST X.T %0 %1 %2 %3 %4 %5 %6 %7 O %9
-:I
-IF %9 == X GOTO INICIO
-IF %9 == x GOTO INICIO
-IF %9 == O GOTO INICIO
-IF %9 == o GOTO INICIO
-IF EXIST X.T %0 %1 %2 %3 %4 %5 %6 %7 %8 X
-IF NOT EXIST X.T %0 %1 %2 %3 %4 %5 %6 %7 %8 O
-REM EN ESTA PARTE SE MIRA SI NADIE GANO, SE DESEA SALIRSE DEL JUEGO Y DE
-REM NO QUERERSE, SE PREGUNTA SI DESEA CONTINUAR O REINICIAR EL JUEGO "
-:FIN
-ECHO.
-ECHO                  ¨ DESEA SALIRSE DEL " BATCHRUN TRIQUI " ? S \ N ?
-:SN
-CODTECLA
-IF ERRORLEVEL 1 IF NOT ERRORLEVEL 31 GOTO SN
-IF ERRORLEVEL 31 IF NOT ERRORLEVEL 32 GOTO SALIR
-IF ERRORLEVEL 32 IF NOT ERRORLEVEL 49 GOTO SN
-:CR
-IF ERRORLEVEL 49 IF NOT ERRORLEVEL 50 ECHO.
-IF ERRORLEVEL 49 IF NOT ERRORLEVEL 50 ECHO            SI DESEA CONTINUAR TECLEE "C" SI DESEA REINICIAR TECLEE "R"
-IF ERRORLEVEL 50 GOTO SN
-CODTECLA
-IF ERRORLEVEL 1 IF NOT ERRORLEVEL 19 GOTO CR
-IF ERRORLEVEL 19 IF NOT ERRORLEVEL 20 DEL *.T
-IF ERRORLEVEL 19 IF NOT ERRORLEVEL 20 ECHO TRIQUI > 0.T
-IF ERRORLEVEL 19 IF NOT ERRORLEVEL 20 %0 A B C D E F G H I
-IF ERRORLEVEL 20 IF NOT ERRORLEVEL 46 GOTO CR
-IF ERRORLEVEL 46 IF NOT ERRORLEVEL 47 GOTO INICIO
-IF ERRORLEVEL 50 GOTO INICIO
-REM " AQUI YA MUESTRA EN PANTALLA UN MENSAJE DANDO LAS GRACIAS POR
-REM HABER USADO EL JUEGO, DESPUES LOS CREDITOS Y LUEGO SE SALE "
-:SALIR
-DEL *.T
-DEL CODTECLA.COM
-ECHO.
-ECHO.
-ECHO.
-ECHO                   GRACIAS POR HABER JUGADO " BATCHRUN TRIQUI "
-PAUSE > NUL
-CLS
-ECHO.
-ECHO.
-ECHO.
-ECHO.
-ECHO.
-ECHO.
-ECHO                            DISE¥ADO POR: BLACKWOLF
-ECHO.
-ECHO.
-ECHO                         TODOS LOS DERECHOS RESERVADOS
-ECHO.
-ECHO                             COLOMBIA COFFEE HACKERS
-PAUSE > NUL
-CLS
+@echo off
+title    Gato   
+color 9
+set suario=0
+set omputador=0
+set mpates=0
+set circulo=O
+set cruz=X
+ 
+:escoje
+cls
+echo.
+echo   Escoje
+echo            1   --   X
+echo            0   --   O
+set /p jugad=     
+if %jugad%==1 goto cru
+if %jugad%==0 (goto circu) else (goto eki)
+ 
+:eki
+echo.
+echo  Esa tecla no es valida
+pause>nul
+goto escoje
+ 
+:cru
+set jugada=X
+set computadores=O
+goto randomize
+ 
+:circu
+set jugada=O
+set computadores=X
+goto randomize
+ 
+:randomize
+set guion=-
+set uno=-
+set dos=-
+set tres=-
+set cuatro=-
+set cinco=-
+set seis=-
+set siete=-
+set ocho=-
+set nueve=-
+set one=1
+set two=2
+set three=3
+set four=4
+set five=5
+set six=6
+set seven=7
+set eight=8
+set nine=9
+set /a rand=%random%
+if %rand% leq 2000 goto compu
+if %rand% leq 4000 goto inicio
+if %rand% leq 6000 goto compu
+if %rand% leq 8000 goto inicio
+if %rand% leq 10000 goto compu
+if %rand% leq 12000 goto inicio
+if %rand% leq 14000 goto compu
+if %rand% leq 16000 goto inicio
+if %rand% leq 18000 goto compu
+if %rand% leq 20000 goto inicio
+if %rand% leq 22000 goto compu
+if %rand% leq 24000 goto inicio
+if %rand% leq 26000 goto compu
+if %rand% leq 28000 goto inicio
+if %rand% leq 30000 goto compu
+if %rand% leq 32000 goto inicio
+if %rand% leq 34000 goto compu
+ 
+:inicio
+cls
+echo.
+echo    " " " " " " " " " " " " " " " " " " "
+echo    "                                   "
+echo    "              |     |              "
+echo    "           %siete%  |  %ocho%  |  %nueve%           "       Estos son los numeros:
+echo    "         _____|_____|_____         "
+echo    "              |     |              "
+echo    "           %cuatro%  |  %cinco%  |  %seis%           "                           7  8  9
+echo    "         _____|_____|_____         "   Usuario    :  %suario%
+echo    "              |     |              "                           4  5  6
+echo    "           %uno%  |  %dos%  |  %tres%           "   Computador :  %omputador%
+echo    "              |     |              "                           1  2  3
+echo    "                                   "   Empates    :  %mpates%
+echo    " " " " " " " " " " " " " " " " " " "
+echo.
+echo.
+if %uno%%dos%%tres%==%computadores%%computadores%%computadores% goto fin_compu
+if %cuatro%%cinco%%seis%==%computadores%%computadores%%computadores% goto fin_compu
+if %siete%%ocho%%nueve%==%computadores%%computadores%%computadores% goto fin_compu
+if %uno%%cuatro%%siete%==%computadores%%computadores%%computadores% goto fin_compu
+if %dos%%cinco%%ocho%==%computadores%%computadores%%computadores% goto fin_compu
+if %tres%%seis%%nueve%==%computadores%%computadores%%computadores% goto fin_compu
+if %cinco%%siete%%tres%==%computadores%%computadores%%computadores% goto fin_compu
+if %nueve%%cinco%%uno%==%computadores%%computadores%%computadores% goto fin_compu
+if %uno%==%guion% goto usuario_juega
+if %dos%==%guion% goto usuario_juega
+if %tres%==%guion% goto usuario_juega
+if %cuatro%==%guion% goto usuario_juega
+if %cinco%==%guion% goto usuario_juega
+if %seis%==%guion% goto usuario_juega
+if %siete%==%guion% goto usuario_juega
+if %ocho%==%guion% goto usuario_juega
+if %nueve%==%guion% (goto usuario_juega) else (goto empate)
+ 
+:usuario_juega
+echo   Juegas con %jugada%
+echo.
+set /p usuario=  elige tu jugada (1-9): 
+if %usuario%==%one% (goto usuario_uno)
+if %usuario%==%two% (goto usuario_dos)
+if %usuario%==%three% (goto usuario_tres)
+if %usuario%==%four% (goto usuario_cuatro)
+if %usuario%==%five% (goto usuario_cinco)
+if %usuario%==%six% (goto usuario_seis)
+if %usuario%==%seven% (goto usuario_siete)
+if %usuario%==%eight% (goto usuario_ocho)
+if %usuario%==%nine% (goto usuario_nueve) else (goto otro)
+ 
+:otro
+echo.
+echo  Esa tecla no es valida
+pause>nul
+goto inicio
+ 
+:usuario_uno
+if %uno%==%circulo% (goto ocu)
+if %uno%==%cruz% (goto ocu)
+set uno=%jugada%
+goto compu
+ 
+:usuario_dos
+if %dos%==%circulo% (goto ocu)
+if %dos%==%cruz% (goto ocu)
+set dos=%jugada%
+goto compu
+ 
+:usuario_tres
+if %tres%==%circulo% (goto ocu)
+if %tres%==%cruz% (goto ocu)
+set tres=%jugada%
+goto compu
+ 
+:usuario_cuatro
+if %cuatro%==%circulo% (goto ocu)
+if %cuatro%==%cruz% (goto ocu)
+set cuatro=%jugada%
+goto compu
+ 
+:usuario_cinco
+if %cinco%==%circulo% (goto ocu)
+if %cinco%==%cruz% (goto ocu)
+set cinco=%jugada%
+goto compu
+ 
+:usuario_seis
+if %seis%==%circulo% (goto ocu)
+if %seis%==%cruz% (goto ocu)
+set seis=%jugada%
+goto compu
+ 
+:usuario_siete
+if %siete%==%circulo% (goto ocu)
+if %siete%==%cruz% (goto ocu)
+set siete=%jugada%
+goto compu
+ 
+:usuario_ocho
+if %ocho%==%circulo% (goto ocu)
+if %ocho%==%cruz% (goto ocu)
+set ocho=%jugada%
+goto compu
+ 
+:usuario_nueve
+if %nueve%==%circulo% (goto ocu)
+if %nueve%==%cruz% (goto ocu)
+set nueve=%jugada%
+goto compu
+ 
+:ocu
+echo.
+echo Esta casilla ya esta ocupada
+pause>nul
+goto inicio
+ 
+:compu
+if %uno%%dos%%tres%==%jugada%%jugada%%jugada% goto fin_usu
+if %cuatro%%cinco%%seis%==%jugada%%jugada%%jugada% goto fin_usu
+if %siete%%ocho%%nueve%==%jugada%%jugada%%jugada% goto fin_usu
+if %uno%%cuatro%%siete%==%jugada%%jugada%%jugada% goto fin_usu
+if %dos%%cinco%%ocho%==%jugada%%jugada%%jugada% goto fin_usu
+if %tres%%seis%%nueve%==%jugada%%jugada%%jugada% goto fin_usu
+if %siete%%cinco%%tres%==%jugada%%jugada%%jugada% goto fin_usu
+if %nueve%%cinco%%uno%==%jugada%%jugada%%jugada% goto fin_usu
+if %uno%%dos%%tres%==%computadores%%computadores%%guion% goto si_tres
+if %uno%%dos%%tres%==%computadores%%guion%%computadores% goto si_dos
+if %uno%%dos%%tres%==%guion%%computadores%%computadores% goto si_uno
+if %cuatro%%cinco%%seis%==%computadores%%computadores%%guion% goto si_seis
+if %cuatro%%cinco%%seis%==%computadores%%guion%%computadores% goto si_cinco
+if %cuatro%%cinco%%seis%==%guion%%computadores%%computadores% goto si_cuatro
+if %siete%%ocho%%nueve%==%computadores%%computadores%%guion% goto si_nueve
+if %siete%%ocho%%nueve%==%computadores%%guion%%computadores% goto si_ocho
+if %siete%%ocho%%nueve%==%guion%%computadores%%computadores% goto si_siete
+if %uno%%cuatro%%siete%==%computadores%%computadores%%guion% goto si_siete
+if %uno%%cuatro%%siete%==%computadores%%guion%%computadores% goto si_cuatro
+if %uno%%cuatro%%siete%==%guion%%computadores%%computadores% goto si_uno
+if %dos%%cinco%%ocho%==%computadores%%computadores%%guion% goto si_ocho
+if %dos%%cinco%%ocho%==%computadores%%guion%%computadores% goto si_cinco
+if %dos%%cinco%%ocho%==%guion%%computadores%%computadores% goto si_dos
+if %tres%%seis%%nueve%==%computadores%%computadores%%guion% goto si_nueve
+if %tres%%seis%%nueve%==%computadores%%guion%%computadores% goto si_seis
+if %tres%%seis%%nueve%==%guion%%computadores%%computadores% goto si_tres
+if %siete%%cinco%%tres%==%computadores%%computadores%%guion% goto si_tres
+if %siete%%cinco%%tres%==%computadores%%guion%%computadores% goto si_cinco
+if %siete%%cinco%%tres%==%guion%%computadores%%computadores% goto si_siete
+if %nueve%%cinco%%uno%==%computadores%%computadores%%guion% goto si_uno
+if %nueve%%cinco%%uno%==%computadores%%guion%%computadores% goto si_cinco
+if %nueve%%cinco%%uno%==%guion%%computadores%%computadores% goto si_nueve
+if %uno%%dos%%tres%==%jugada%%jugada%%guion% goto si_tres
+if %uno%%dos%%tres%==%jugada%%guion%%jugada% goto si_dos
+if %uno%%dos%%tres%==%guion%%jugada%%jugada% goto si_uno
+if %cuatro%%cinco%%seis%==%jugada%%jugada%%guion% goto si_seis
+if %cuatro%%cinco%%seis%==%jugada%%guion%%jugada% goto si_cinco
+if %cuatro%%cinco%%seis%==%guion%%jugada%%jugada% goto si_cuatro
+if %siete%%ocho%%nueve%==%jugada%%jugada%%guion% goto si_nueve
+if %siete%%ocho%%nueve%==%jugada%%guion%%jugada% goto si_ocho
+if %siete%%ocho%%nueve%==%guion%%jugada%%jugada% goto si_siete
+if %uno%%cuatro%%siete%==%jugada%%jugada%%guion% goto si_siete
+if %uno%%cuatro%%siete%==%jugada%%guion%%jugada% goto si_cuatro
+if %uno%%cuatro%%siete%==%guion%%jugada%%jugada% goto si_uno
+if %dos%%cinco%%ocho%==%jugada%%jugada%%guion% goto si_ocho
+if %dos%%cinco%%ocho%==%jugada%%guion%%jugada% goto si_cinco
+if %dos%%cinco%%ocho%==%guion%%jugada%%jugada% goto si_dos
+if %tres%%seis%%nueve%==%jugada%%jugada%%guion% goto si_nueve
+if %tres%%seis%%nueve%==%jugada%%guion%%jugada% goto si_seis
+if %tres%%seis%%nueve%==%guion%%jugada%%jugada% goto si_tres
+if %siete%%cinco%%tres%==%jugada%%jugada%%guion% goto si_tres
+if %siete%%cinco%%tres%==%jugada%%guion%%jugada% goto si_cinco
+if %siete%%cinco%%tres%==%guion%%jugada%%jugada% goto si_siete
+if %nueve%%cinco%%uno%==%jugada%%jugada%%guion% goto si_uno
+if %nueve%%cinco%%uno%==%jugada%%guion%%jugada% goto si_cinco
+if %nueve%%cinco%%uno%==%guion%%jugada%%jugada% goto si_nueve
+if %uno%==- goto computador
+if %dos%==- goto computador
+if %tres%==- goto computador
+if %cuatro%==- goto computador
+if %cinco%==- goto computador
+if %seis%==- goto computador
+if %siete%==- goto computador
+if %ocho%==- goto computador
+if %nueve%==- (goto computador) else (goto empate)
+ 
+:computador
+set /a x=%random%
+if %x% leq 3640 goto siete
+if %x% leq 7280 goto ocho
+if %x% leq 10920 goto seis
+if %x% leq 14560 goto uno
+if %x% leq 18200 goto cinco
+if %x% leq 21840 goto nueve
+if %x% leq 25480 goto cuatro
+if %x% leq 29120 goto tres
+if %x% leq 32767 goto dos
+ 
+:uno
+if %uno%==%guion% (goto si_uno) else (goto compu)
+:dos
+if %dos%==%guion% (goto si_dos) else (goto compu)
+:tres
+if %tres%==%guion% (goto si_tres) else (goto compu)
+ 
+:cuatro
+if %cuatro%==%guion% (goto si_cuatro) else (goto compu)
+ 
+:cinco
+if %cinco%==%guion% (goto si_cinco) else (goto compu)
+ 
+:seis
+if %seis%==%guion% (goto si_seis) else (goto compu)
+ 
+:siete
+if %siete%==%guion% (goto si_siete) else (goto compu)
+ 
+:ocho
+if %ocho%==%guion% (goto si_ocho) else (goto compu)
+ 
+:nueve
+if %nueve%==%guion% (goto si_nueve) else (goto compu)
+ 
+:si_uno
+set uno=%computadores%
+goto inicio
+ 
+:si_dos
+set dos=%computadores%
+goto inicio
+ 
+:si_tres
+set tres=%computadores%
+goto inicio
+ 
+:si_cuatro
+set cuatro=%computadores%
+goto inicio
+ 
+:si_cinco
+set cinco=%computadores%
+goto inicio
+ 
+:si_seis
+set seis=%computadores%
+goto inicio
+ 
+:si_siete
+set siete=%computadores%
+goto inicio
+ 
+:si_ocho
+set ocho=%computadores%
+goto inicio
+ 
+:si_nueve
+set nueve=%computadores%
+goto inicio
+ 
+:fin_compu
+echo.
+echo       Game over
+echo.
+set /a omputador=omputador+1
+echo.
+echo   Presione una tecla para volver a jugar
+pause>nul
+goto randomize
+ 
+:fin_usu
+echo.
+echo       You Win
+echo.
+set /a suario=suario+1
+echo.
+echo   Presione una tecla para volver a jugar
+pause>nul
+goto randomize
+ 
+:empate
+echo.
+echo      Empate
+set /a mpates=mpates+1
+echo.
+echo   Presione una tecla para volver a jugar
+pause>nul
+goto randomize
